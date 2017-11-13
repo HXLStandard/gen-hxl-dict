b1d7666ba873afe70ccceb7fc5adaef5535391b5
David Megginson <david.megginson@megginson.com>
2017-11-13 10:53:01 -0500

    Created empty script and setup.py to pull in dependencies.

---
+++ gen-hxl-dict.py
@@ -0,0 +1,3 @@
+import libhxl
+
+print("Hello, HXL!")
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name='gen-hxl-ict',
+      version='0.1',
+      description='Python script to generate HTML HXL hashtag dictionary.',
+      author='David Megginson',
+      author_email='contact@megginson.com',
+      install_requires=['libhxl'],
+)
+
